e7c699ff59eff885d67eade4bf92de7b4d22fcd5
Afshin Mokhtari <afshinator@hotmail.com>
2018-04-12 17:36:56 -0700

    add redux

--- src/components/TaskList.stories.js
+++ src/components/TaskList.stories.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 
+import { PureTaskList } from './TaskList';
 import { task, actions } from './Task.stories';
-import TaskList from './TaskList';
 
 export const defaultTasks = [
   { ...task, id: '1', title: 'Task 1' },
@@ -20,7 +20,7 @@ export const withPinnedTasks = [
 
 storiesOf('TaskList', module)
   .addDecorator(story => <div style={{ padding: '3rem' }}>{story()}</div>)
-  .add('default', () => <TaskList tasks={defaultTasks} {...actions} />)
-  .add('withPinnedTasks', () => <TaskList tasks={withPinnedTasks} {...actions} />)
-  .add('loading', () => <TaskList loading tasks={[]} {...actions} />)
-  .add('empty', () => <TaskList tasks={[]} {...actions} />);
+  .add('default', () => <PureTaskList tasks={defaultTasks} {...actions} />)
+  .add('withPinnedTasks', () => <PureTaskList tasks={withPinnedTasks} {...actions} />)
+  .add('loading', () => <PureTaskList loading tasks={[]} {...actions} />)
+  .add('empty', () => <PureTaskList tasks={[]} {...actions} />);
